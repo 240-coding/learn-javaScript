b0dc35692616ef15ef4522454f423d698948e4a1
240 <wow9176@naver.com>
2021-01-23 02:26:29 +0900

    03 - 변수 자료형 연산자

---
+++ doit_javascript_basic-master/03/ex-1.js
@@ -0,0 +1,2 @@
+var word = prompt("내용 입렭 : ", "자바스크립트");
+document.write(word);
---
+++ doit_javascript_basic-master/03/ex-2.js
@@ -0,0 +1,3 @@
+var width = prompt("사각형의 가로 값 : ");
+var height = prompt("사각형의 세로 값 : ");
+alert("주어진 사각형의 너비 : " + (width * height));
