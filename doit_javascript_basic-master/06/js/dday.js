fe491a89e270905a7de5fcdbbf08d7e5fab74bf6
240 <wow9176@naver.com>
2021-01-28 17:51:06 +0900

    calcDate

--- doit_javascript_basic-master/06/js/dday.js
+++ doit_javascript_basic-master/06/js/dday.js
@@ -9,20 +9,21 @@ var passedDay = Math.round(passedTime/(1000 * 60 * 60 * 24));
 
 document.querySelector("#accent").innerText = passedDay + "일";
 
-var future = toFirst + 100 * (1000 * 60 * 60 * 24);
-var someday = new Date(future);
-
-var year = someday.getFullYear();
-var month = someday.getMonth();
-var date = someday.getDate();
-
-document.querySelector("#date100").innerText = year + "년 " + month + "월 " + date + "일";
-
-var future = toFirst + 200 * (1000 * 60 * 60 * 24);
-var someday = new Date(future);
-
-var year = someday.getFullYear();
-var month = someday.getMonth();
-var date = someday.getDate();
+calcDate(100);
+calcDate(200);
+calcDate(365);
+calcDate(500);
 
 document.querySelector("#date200").innerText = year + "년 " + month + "월 " + date + "일";
+
+function calcDate(days) {
+    var future = toFirst + days * (1000 * 60 * 60 * 24);
+    var someday = new Date(future);
+
+    var year = someday.getFullYear();
+    var month = someday.getMonth();
+    var date = someday.getDate();
+
+    document.querySelector("#date" + days).innerText = year + "년 " + month + "월 " + date + "일";
+
+}
