04f41ae11f4d0b6cf0fa5cafa0514f6aaa25a699
240 <wow9176@naver.com>
2021-02-08 05:14:26 +0900

    09 - quiz2 (2)

--- doit_javascript_basic-master/09/js/quiz-2.js
+++ doit_javascript_basic-master/09/js/quiz-2.js
@@ -1,11 +1,10 @@
-var radius = document.querySelector("#radius");
-var round = document.querySelector("#round");
-var area = document.querySelector("#area");
 
 var start = document.querySelector("#start");
 var pi = Math.PI;
 
 start.addEventListener("click", function() {
-    round.value = radius.value * 2 * pi;
-    area.value = radius.value * radius.value * pi;
+    var radius = document.querySelector("#radius").value;
+
+    document.querySelector("#round").value = radius * 2 * pi;
+    document.querySelector("#area").value = radius * radius * pi;
 })
