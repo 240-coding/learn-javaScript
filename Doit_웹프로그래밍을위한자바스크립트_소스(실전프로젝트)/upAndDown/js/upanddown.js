8d264ddf8c935185ba4a5bd0ef8e3b9ec790f72d
240 <wow9176@naver.com>
2021-02-10 15:33:24 +0900

    01 - 시도 횟수 추가

--- Doit_웹프로그래밍을위한자바스크립트_소스(실전프로젝트)/upAndDown/js/upanddown.js
+++ Doit_웹프로그래밍을위한자바스크립트_소스(실전프로젝트)/upAndDown/js/upanddown.js
@@ -2,24 +2,29 @@ var comNum = Math.floor(Math.random() * 100 + 1); // 컴퓨터가 만든 숫자
 var userLabel = document.querySelector("#try");
 var display = document.querySelector("#display");
 var counter = document.querySelector("#counter");
+var tryCount = 0; // 시도 횟수
 
-function playGame() {
-    var userNum = Number(userLabel.value);
+function playGame() { // 확인 버튼 클릭시 실행
+    var userNum = Number(userLabel.value); // 사용자가 입력한 숫자
 
-    if (userNum < 1 || userNum > 100) {
+    if (userNum < 1 || userNum > 100) { // 범위를 넘어선 경우
         alert("1~100 사이의 숫자만 입력할 수 있습니다.");
     }
     else {
-        if (userNum < comNum) {
+        if (userNum < comNum) { // 정답보다 작은 경우
             display.innerText = "UP";
             userLabel.value = "";
+            counter.innerText = "시도 횟수 : " + ++tryCount + "회";
         }
-        else if (userNum > comNum) {
+        else if (userNum > comNum) { // 정답보다 큰 경우
             display.innerText = "DOWN";
             userLabel.value = "";
+            counter.innerText = "시도 횟수 : " + ++tryCount + "회";
         }
-        else {
+        else { // 정답을 맞힌 경우
             display.innerText = "맞힘";
+            userLabel.value = "";
+            counter.innerText = "시도 횟수 : " + ++tryCount + "회";
         }
     }
 }
