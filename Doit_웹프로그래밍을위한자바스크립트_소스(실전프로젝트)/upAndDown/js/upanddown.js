5312eab727db7f9a1b347897973c6f2cb9448b33
240 <wow9176@naver.com>
2021-02-10 16:55:59 +0900

    01 - 키보드 이벤트

--- Doit_웹프로그래밍을위한자바스크립트_소스(실전프로젝트)/upAndDown/js/upanddown.js
+++ Doit_웹프로그래밍을위한자바스크립트_소스(실전프로젝트)/upAndDown/js/upanddown.js
@@ -4,6 +4,13 @@ var display = document.querySelector("#display");
 var counter = document.querySelector("#counter");
 var tryCount = 0; // 시도 횟수
 
+userLabel.onkeydown = function(e) {
+    if (e.keyCode == 13 || e.which == 3) {
+        playGame();
+        return false;
+    }
+}
+
 function playGame() { // 확인 버튼 클릭시 실행
     var userNum = Number(userLabel.value); // 사용자가 입력한 숫자
 
@@ -13,18 +20,14 @@ function playGame() { // 확인 버튼 클릭시 실행
     else {
         if (userNum < comNum) { // 정답보다 작은 경우
             display.innerText = "UP";
-            userLabel.value = "";
-            counter.innerText = "시도 횟수 : " + ++tryCount + "회";
         }
         else if (userNum > comNum) { // 정답보다 큰 경우
             display.innerText = "DOWN";
-            userLabel.value = "";
-            counter.innerText = "시도 횟수 : " + ++tryCount + "회";
         }
         else { // 정답을 맞힌 경우
             display.innerText = "맞힘";
-            userLabel.value = "";
-            counter.innerText = "시도 횟수 : " + ++tryCount + "회";
         }
     }
+    userLabel.value = "";
+    counter.innerText = "시도 횟수 : " + ++tryCount + "회";
 }
